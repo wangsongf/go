9a8f2401e0e9ca992da7a4a55786e1a53c2e613f
王松峰 <oumatsubon@wangsongfengdeMacBook-Pro.local>
2018-06-14 23:02:18 +0800

    echo 学习

---
+++ echo/server.php
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+	
+	"github.com/labstack/echo"
+)
+
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
+// e.GET("/users/:id", getUser)
+func getUser(c echo.Context) error {
+  	// User ID from path `users/:id`
+  	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+//e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + ", member:" + member)
+}
+
+// e.POST("/save", save)
+func save(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:" + name + ", email:" + email)
+}
+
+func main() {
+	e := echo.New()
+
+	
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.POST("/users", func(c echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
+	//e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	//e.PUT("/users/:id", updateUser)
+	//e.DELETE("/users/:id", deleteUser)
+	e.POST("/save", save)
+	e.GET("/show", show)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
